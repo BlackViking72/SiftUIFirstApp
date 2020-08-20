8dc89a188483c03f6e5325b1b2d956cb6b1e0552
Het Patel <hetp721011@gmail.com>
2020-08-20 22:21:57 +0530

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Het Patel on 20/08/20.
+//  Copyright © 2020 Het Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
