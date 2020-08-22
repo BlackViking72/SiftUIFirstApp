4086ff26e6eeb86f0430c04e89b3350423f9950e
Het Patel <hetp721011@gmail.com>
2020-08-22 12:03:17 +0530

    Initial Commit

---
+++ FirstApp/MenuView.swift
@@ -0,0 +1,22 @@
+//
+//  MenuView.swift
+//  FirstApp
+//
+//  Created by Het Patel on 22/08/20.
+//  Copyright © 2020 Het Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Account")
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
